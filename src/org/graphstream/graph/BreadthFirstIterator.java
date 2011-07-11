8f8d4b0623422d76f1420413c82c63a5767ae379
Ant01n3 <antoine.dutot@gmail.com>
2011-07-11 15:08:08 +0200

    Corrected a bug in BreathFirstIterator.
    This iterator used LinkedList.contains() to know if a node was
    processed or not, which is ..... very slow !
    
    Also added the possibility to obtain the depth of each node and
    the depth max, this can be used to compute the diameter of the
    graph.

--- src/org/graphstream/graph/BreadthFirstIterator.java
+++ src/org/graphstream/graph/BreadthFirstIterator.java
@@ -30,13 +30,23 @@
  */
 package org.graphstream.graph;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Node;
+
 /**
  * Iterator allowing to explore a graph in a breadth-first way.
+ * 
+ * This iterator also allows to compute the depth or each node (number of edges
+ * crossed to reach a node from the starting node) as well as the maximum
+ * depth. Be careful that this depth does not take eventual weights of edges,
+ * it only counts edges. The depth of each node and the depth max are known
+ * only when all the nodes have been processed by this iterator (only when
+ * {@link #hasNext()} returns false). 
  * 
  * @complexity O(n+m) with n the number of nodes and m the number of edges.
  * @since 20040730
@@ -52,7 +62,7 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	/**
 	 * Already processed nodes.
 	 */
-	protected HashSet<T> closed = new HashSet<T>();
+	protected HashMap<T,Integer> closed = new HashMap<T,Integer>();
 
 	/**
 	 * Nodes remaining to process. The iteration continues as long as this array
@@ -60,6 +70,11 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	 */
 	protected LinkedList<T> open = new LinkedList<T>();
 
+	/**
+	 * Maximum depth.
+	 */
+	protected int depthMax = 0;
+	
 	// Constructors
 
 	/**
@@ -83,6 +98,7 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	 */
 	public BreadthFirstIterator(T startNode, boolean directed) {
 		open.add(startNode);
+		closed.put(startNode,0);
 		this.directed = directed;
 	}
 
@@ -96,16 +112,49 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	public boolean isDirected() {
 		return directed;
 	}
-
+	
+	/**
+	 * Depth of the more distant node from the starting point of the BFS, this is also known as
+	 * the eccentricity if the graph non-weighted. This method can only be used reliably after all
+	 * the nodes have been processed.
+	 * 
+	 * @return The maximum depth of a node from the starting node (in edge jumps).
+	 */
+	public int getDepthMax() {
+		return depthMax;
+	}
+	
+	/**
+	 * Depth of a given node. The depth here is minimal number of edges to cross to reach
+	 * the given node from the starting node of the BFS. Weights are not used. 
+	 * This method can only be used reliably after all the nodes
+	 * have been processed, if the depth has not been computed -1 is returned.
+	 * 
+	 * @param node
+	 * 			The node for which the depth is required.
+	 * @return The depth of the given node.
+	 */
+	public int getDepthOf(Node node) {
+		Integer i = closed.get(node);
+		
+		if(i != null)
+			return (int)i;
+		
+		return -1;
+	}
+	
 	/**
 	 * Is the given node tabu?.
+	 * 
+	 * Tabu nodes are nodes that have already been discovered (and are therefore either
+	 * in closed or open or both).
 	 * 
 	 * @param node
 	 *            The node to test.
 	 * @return True if tabu.
 	 */
 	protected boolean tabu(T node) {
-		return (closed.contains(node) || open.contains(node));
+		return closed.containsKey(node);
 	}
 
 	/**
@@ -118,7 +167,7 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	}
 
 	/**
-	 * Next node to process.
+	 * Next node to process, in BF order.
 	 * 
 	 * @return The next node.
 	 */
@@ -127,7 +176,6 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 			T next = open.removeFirst();
 
 			addNeighborsOf(next);
-			closed.add(next);
 
 			return next;
 		} else {
@@ -136,7 +184,7 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 	}
 
 	/**
-	 * Append the neighbors of the given node (excepted nodes already processed)
+	 * Append the neighbors of the given node (excepted nodes already processed/marked)
 	 * in the list of nodes to process next.
 	 * 
 	 * @param node
@@ -150,13 +198,22 @@ public class BreadthFirstIterator<T extends Node> implements Iterator<T> {
 		else
 			k = node.getEdgeIterator();
 
+		boolean found = false;
+		int curDepth = ((int)closed.get(node)) + 1;
+		
 		while (k.hasNext()) {
 			Edge edge = k.next();
 			T adj = edge.getOpposite(node);
 
-			if (!tabu(adj))
+			if (!tabu(adj)) {
 				open.add(adj);
+				closed.put(adj, curDepth);
+				found = true;
+			}
 		}
+		
+		if(found && curDepth > depthMax)
+			depthMax = curDepth;
 	}
 
 	// Commands
