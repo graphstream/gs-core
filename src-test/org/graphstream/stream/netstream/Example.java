7314a8db9d81515ba5f0e2cfad4d03b5d2ba8e5f
pigne <yoann.pigne@gmail.com>
2011-08-17 15:35:16 +0200

    Unit Tests plus and example for the NetStream protocol

---
+++ src-test/org/graphstream/stream/netstream/Example.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ * Copyright (c) 2011 University of Luxembourg
+ * 
+ * @file Example.java
+ * @date Aug 17, 2011
+ * 
+ * @author Yoann Pigné
+ * 
+ */
+package org.graphstream.stream.netstream;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.stream.thread.ThreadProxyPipe;
+
+/**
+ * A simple example of use of the NetStream sender a receiver.
+ * <p>
+ * This is a dummy example where the Java Sender and Receiver are used together.
+ * This is not the normal use of the NetStream, normally the receiver should be
+ * connected to a sender from another language or the opposite. For a
+ * java-to-java network communication other tools based on RMI are probably more
+ * suitable.
+ * </p>
+ * <p>
+ * In this class a basic receiver plugged to a displayed multigraph is created
+ * and waits for events on the "default" stream, on port 2001 at "localhost". In
+ * another thread, a Sender is created with a graph connected to its sink, so
+ * that any event from the graph will end-up to the sender. The sender connects
+ * to "localhost" on port 2001 and sends, on the default stream, any received event to the receiver,
+ * through the network, using the NetStream protocol.
+ * </p>
+ */
+public class Example {
+
+	public Example() throws UnknownHostException, IOException, InterruptedException {
+
+		// ----- On the receiver side -----
+		//
+		// - a graph that will display the received events
+		Graph g = new MultiGraph("G");
+		g.display();
+		// - the receiver that waits for events
+		NetStreamReceiver net = new NetStreamReceiver(2001);
+		// - received events end up in the "default" pipe
+		ThreadProxyPipe pipe = net.getDefaultStream();
+		// - plug the pipe to the sink of the graph
+		pipe.addSink(g);
+
+		// ----- The sender side (in another thread) ------
+		//
+		new Thread() {
+
+			public void run() {
+				// - the original graph from which events are generated
+				Graph g = new MultiGraph("G");
+				// - the sender
+				NetStreamSender nsc = null;
+				try {
+					nsc = new NetStreamSender(2001);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				// - plug the graph to the sender so that graph events can be sent automatically
+				g.addSink(nsc);
+				// - generate some events on the cleint side
+				String style = "node{fill-mode:plain;fill-color:#567;size:6px;}";
+				g.addAttribute("stylesheet", style);
+				g.addAttribute("ui.antialias", true);
+				g.addAttribute("layout.stabilization-limit", 0);
+				for (int i = 0; i < 500; i++) {
+					g.addNode(i + "");
+					if (i > 0) {
+						g.addEdge(i + "-" + (i - 1), i + "", (i - 1) + "");
+						g.addEdge(i + "--" + (i / 2), i + "", (i / 2) + "");
+					}
+				}
+			}
+		}.start();
+		
+		// ----- Back to the receiver side -----
+		//
+		// -The receiver pro-actively checks for events on the ThreadProxyPipe
+		while(true){
+			pipe.pump();
+			Thread.sleep(100);
+		}
+		
+	}
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		new Example();
+	}
+}
---
+++ src-test/org/graphstream/stream/netstream/TestNetStream.java
@@ -0,0 +1,491 @@
+/**
+ * 
+ * Copyright (c) 2010 University of Luxembourg
+ * 
+ * @file TestNetStream.java
+ * @date Aug 16, 2011
+ * 
+ * @author Yoann Pigné
+ * 
+ */
+package org.graphstream.stream.netstream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Vector;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.stream.Sink;
+import org.graphstream.stream.SinkAdapter;
+import org.graphstream.stream.thread.ThreadProxyPipe;
+import org.junit.Test;
+
+/**
+ * Test of the NetStream protocol, sender and receiver
+ */
+public class TestNetStream {
+
+	Vector<String> errors;
+
+	/**
+	 * Tests (almost) all the possible data types encoding and decoding.
+	 */
+	@Test
+	public void testNetStreamTypes() {
+
+		NetStreamReceiver net = null;
+		try {
+			net = new NetStreamReceiver("localhost", 2001, true);
+		} catch (UnknownHostException e1) {
+			fail(e1.toString());
+		} catch (IOException e1) {
+			fail(e1.toString());
+		}
+
+		ThreadProxyPipe pipe = net.getDefaultStream();
+
+		pipe.addSink(new SinkAdapter() {
+			public void graphAttributeAdded(String sourceId, long timeId,
+					String attribute, Object value) {
+				validate(attribute, value);
+			}
+			public void graphAttributeChanged(String sourceId, long timeId,
+					String attribute, Object oldValue, Object newValue) {
+				validate(attribute, newValue);
+			}
+			private void validate(String attribute, Object value) {
+				String valueType = null;
+				@SuppressWarnings("rawtypes")
+				Class valueClass = value.getClass();
+				boolean isArray = valueClass.isArray();
+				if (isArray) {
+					valueClass = ((Object[]) value)[0].getClass();
+				}
+				if (valueClass.equals(Boolean.class)) {
+					if (isArray) {
+						valueType = "booleanArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Boolean[]) value));
+
+					} else {
+						valueType = "boolean";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Byte.class)) {
+					if (isArray) {
+						valueType = "byteArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Byte[]) value));
+					} else {
+						valueType = "byte";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Short.class)) {
+					if (isArray) {
+						valueType = "shortArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Short[]) value));
+					} else {
+						valueType = "short";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Integer.class)) {
+					if (isArray) {
+						valueType = "intArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Integer[]) value));
+					} else {
+						valueType = "int";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Long.class)) {
+					if (isArray) {
+						valueType = "longArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Long[]) value));
+					} else {
+						valueType = "long";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Float.class)) {
+					if (isArray) {
+						valueType = "floatArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Float[]) value));
+					} else {
+						valueType = "float";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(Double.class)) {
+					if (isArray) {
+						valueType = "doubleArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Double[]) value));
+					} else {
+						valueType = "double";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				} else if (valueClass.equals(String.class)) {
+					if (isArray) {
+						valueType = "typeArray";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute,
+								Arrays.toString((Boolean[]) value));
+					} else {
+						valueType = "string";
+						System.out.printf("found a %s for attribute %s=%s%n",
+								valueType, attribute, value.toString());
+					}
+				}
+
+				assertTrue(valueType.equals(attribute));
+
+			}
+
+		});
+
+		new Thread() {
+
+			@Override
+			public void run() {
+
+				Graph g = new MultiGraph("G");
+				NetStreamSender nsc = null;
+				try {
+					nsc = new NetStreamSender("localhost", 2001);
+				} catch (UnknownHostException e1) {
+					error(e1.toString());
+					return;
+				} catch (IOException e1) {
+					error(e1.toString());
+					return;
+				}
+				g.addSink(nsc);
+
+				g.addAttribute("intArray", 0, Integer.MAX_VALUE,
+						Integer.MIN_VALUE);
+				g.addAttribute("floatArray", 0f, Float.MAX_VALUE,
+						Float.MIN_VALUE);
+				g.addAttribute("doubleArray", 0.0, Double.MAX_VALUE,
+						Double.MIN_VALUE);
+				g.addAttribute("shortArray", (short) 0, Short.MAX_VALUE,
+						Short.MIN_VALUE);
+				g.addAttribute("longArray", 0L, Long.MAX_VALUE, Long.MIN_VALUE);
+				g.addAttribute("byteArray", (byte) 0, Byte.MAX_VALUE,
+						Byte.MIN_VALUE);
+				g.addAttribute("booleanArray", true, false);
+				// Object[] three = {new Short((short) 3),new Long(3L),"3"};
+				// g.addAttribute("typeArray","one", 2 , three);
+				g.addAttribute("int", 1);
+				g.addAttribute("float", 1f);
+				g.addAttribute("double", 1.0);
+				g.addAttribute("short", (short) 0);
+				g.addAttribute("long", 1L);
+				g.addAttribute("byte", (byte) 0);
+				g.addAttribute("boolean", true);
+				g.addAttribute("string", "true");
+				try {
+					nsc.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		pipe.pump();
+
+		if (errors != null) {
+			for (String s : errors) {
+				System.err.println(s);
+				fail(s);
+			}
+		}
+		net.quit();
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Test multiple senders running on separated threads. They target different
+	 * and/or same streams of the same receiver.
+	 * 
+	 * <ul>
+	 * <li>Sender 1 -> Stream 1</li>
+	 * <li>Sender 2 -> Stream 1</li>
+	 * <li>Sender 3 -> Stream 2</li>
+	 * </ul>
+	 */
+	@Test
+	public void testNetStreamMultiThreadSenders() {
+		Graph g1 = new MultiGraph("G1");
+		Graph g2 = new MultiGraph("G2");
+		NetStreamReceiver net = null;
+		try {
+			net = new NetStreamReceiver("localhost", 2002, true);
+		} catch (UnknownHostException e1) {
+			fail(e1.toString());
+		} catch (IOException e1) {
+			fail(e1.toString());
+		}
+
+		ThreadProxyPipe pipe1 = net.getStream("G1");
+		ThreadProxyPipe pipe2 = net.getStream("G2");
+
+		pipe1.addSink(g1);
+		pipe2.addSink(g2);
+
+		launchClient(2002, "G1", "0");
+		launchClient(2002, "G1", "1");
+		launchClient(2002, "G2", "0");
+
+		for (int i = 0; i < 10; i++) {
+			pipe1.pump();
+			pipe2.pump();
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		pipe1.pump();
+		pipe2.pump();
+
+		assertEquals("G1", g1.getAttribute("id"));
+		assertEquals("G2", g2.getAttribute("id"));
+		assertEquals(180, g1.getNodeCount());
+		assertEquals(90, g2.getNodeCount());
+
+	}
+
+	private void launchClient(final int port, final String id,
+			final String prefix) {
+		new Thread() {
+
+			@Override
+			public void run() {
+
+				Graph g = new MultiGraph(id);
+
+				NetStreamSender nsc = null;
+				try {
+					nsc = new NetStreamSender(id, "localhost", port);
+				} catch (UnknownHostException e1) {
+					error(e1.toString());
+					return;
+				} catch (IOException e1) {
+					error(e1.toString());
+					return;
+				}
+				g.addSink(nsc);
+
+				g.addAttribute("id", id);
+
+				for (int i = 0; i < 30; i++) {
+					g.addNode(prefix + i + "_1");
+					g.addNode(prefix + i + "_0");
+					g.addNode(prefix + i + "_2");
+					try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					nsc.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+
+	@Test
+	public void testNetStreamEvents() {
+		Graph g1 = new MultiGraph("G");
+		NetStreamReceiver net = null;
+		try {
+			net = new NetStreamReceiver("localhost", 2003, true);
+		} catch (UnknownHostException e1) {
+			fail(e1.toString());
+		} catch (IOException e1) {
+			fail(e1.toString());
+		}
+
+		ThreadProxyPipe pipe = net.getDefaultStream();
+
+		pipe.addSink(g1);
+
+		g1.addSink(new Sink() {
+
+			@Override
+			public void graphAttributeAdded(String sourceId, long timeId,
+					String attribute, Object value) {
+				assertEquals(0, value);
+				assertEquals("graphAttribute", attribute);
+			}
+			@Override
+			public void graphAttributeChanged(String sourceId, long timeId,
+					String attribute, Object oldValue, Object newValue) {
+				assertTrue((Integer) newValue == 0 || (Integer) newValue == 1);
+				assertEquals("graphAttribute", attribute);
+			}
+			@Override
+			public void graphAttributeRemoved(String sourceId, long timeId,
+					String attribute) {
+				assertEquals("graphAttribute", attribute);
+			}
+			@Override
+			public void nodeAttributeAdded(String sourceId, long timeId,
+					String nodeId, String attribute, Object value) {
+				assertEquals(0, value);
+				assertEquals("nodeAttribute", attribute);
+			}
+			@Override
+			public void nodeAttributeChanged(String sourceId, long timeId,
+					String nodeId, String attribute, Object oldValue,
+					Object newValue) {
+				assertTrue((Integer) newValue == 0 || (Integer) newValue == 1);
+				assertEquals("nodeAttribute", attribute);
+			}
+			@Override
+			public void nodeAttributeRemoved(String sourceId, long timeId,
+					String nodeId, String attribute) {
+				assertEquals("nodeAttribute", attribute);
+			}
+			@Override
+			public void edgeAttributeAdded(String sourceId, long timeId,
+					String edgeId, String attribute, Object value) {
+				assertEquals(0, value);
+				assertEquals("edgeAttribute", attribute);
+			}
+			@Override
+			public void edgeAttributeChanged(String sourceId, long timeId,
+					String edgeId, String attribute, Object oldValue,
+					Object newValue) {
+				assertTrue((Integer) newValue == 0 || (Integer) newValue == 1);
+				assertEquals("edgeAttribute", attribute);
+			}
+			@Override
+			public void edgeAttributeRemoved(String sourceId, long timeId,
+					String edgeId, String attribute) {
+				assertEquals("edgeAttribute", attribute);
+			}
+			@Override
+			public void nodeAdded(String sourceId, long timeId, String nodeId) {
+				assertTrue("node0".equals(nodeId) || "node1".equals(nodeId));
+			}
+			@Override
+			public void nodeRemoved(String sourceId, long timeId, String nodeId) {
+				assertTrue("node0".equals(nodeId) || "node1".equals(nodeId));
+			}
+			@Override
+			public void edgeAdded(String sourceId, long timeId, String edgeId,
+					String fromNodeId, String toNodeId, boolean directed) {
+				assertEquals("edge", edgeId);
+				assertEquals("node0", fromNodeId);
+				assertEquals("node1", toNodeId);
+				assertEquals(true, directed);
+			}
+			@Override
+			public void edgeRemoved(String sourceId, long timeId, String edgeId) {
+				assertEquals("edge", edgeId);
+			}
+			@Override
+			public void graphCleared(String sourceId, long timeId) {
+			}
+			@Override
+			public void stepBegins(String sourceId, long timeId, double step) {
+				assertEquals(1.1, step);
+			}
+		});
+
+		new Thread() {
+
+			@Override
+			public void run() {
+
+				Graph g = new MultiGraph("G", false, true);
+
+				NetStreamSender nsc = null;
+				try {
+					nsc = new NetStreamSender("localhost", 2003);
+				} catch (UnknownHostException e1) {
+					error(e1.toString());
+					return;
+				} catch (IOException e1) {
+					error(e1.toString());
+					return;
+				}
+				g.addSink(nsc);
+				Node node0 = g.addNode("node0");
+				// Node node1 = g.addNode("node1");
+				Edge edge = g.addEdge("edge", "node0", "node1", true);
+				node0.addAttribute("nodeAttribute", 0);
+				node0.changeAttribute("nodeAttribute", 1);
+				node0.removeAttribute("nodeAttribute");
+				edge.addAttribute("edgeAttribute", 0);
+				edge.changeAttribute("edgeAttribute", 1);
+				edge.removeAttribute("edgeAttribute");
+				g.addAttribute("graphAttribute", 0);
+				g.changeAttribute("graphAttribute", 1);
+				g.removeAttribute("graphAttribute");
+				g.stepBegins(1.1);
+
+			}
+		}.start();
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		pipe.pump();
+
+	}
+
+	synchronized void error(String s) {
+		if (errors == null) {
+			errors = new Vector<String>();
+		}
+		errors.add(s);
+	}
+
+	public static void main(String[] args) {
+		new TestNetStream().testNetStreamEvents();
+	}
+}
