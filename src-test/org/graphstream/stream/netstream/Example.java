33d9cc0ef79966e14b749795c3a2066e33e9f534
pigne <yoann.pigne@gmail.com>
2011-08-29 15:59:54 +0200

    Javadoc + examples in the test-src dir

--- src-test/org/graphstream/stream/netstream/Example.java
+++ src-test/org/graphstream/stream/netstream/Example.java
@@ -1,13 +1,35 @@
-/**
+/*
+ * Copyright 2006 - 2011 
+ *     Julien Baudry	<julien.baudry@graphstream-project.org>
+ *     Antoine Dutot	<antoine.dutot@graphstream-project.org>
+ *     Yoann Pigné		<yoann.pigne@graphstream-project.org>
+ *     Guilhelm Savin	<guilhelm.savin@graphstream-project.org>
  * 
- * Copyright (c) 2011 University of Luxembourg
+ * This file is part of GraphStream <http://graphstream-project.org>.
  * 
- * @file Example.java
- * @date Aug 17, 2011
+ * GraphStream is a library whose purpose is to handle static or dynamic
+ * graph, create them from scratch, file or any source and display them.
  * 
- * @author Yoann Pigné
+ * This program is free software distributed under the terms of two licenses, the
+ * CeCILL-C license that fits European law, and the GNU Lesser General Public
+ * License. You can  use, modify and/ or redistribute the software under the terms
+ * of the CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
+ * URL <http://www.cecill.info> or under the terms of the GNU LGPL as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
  */
+
+
 package org.graphstream.stream.netstream;
 
 import java.io.IOException;
@@ -34,6 +56,14 @@ import org.graphstream.stream.thread.ThreadProxyPipe;
  * to "localhost" on port 2001 and sends, on the default stream, any received
  * event to the receiver, through the network, using the NetStream protocol.
  * </p>
+ * 
+ * Copyright (c) 2011 University of Luxembourg
+ * 
+ * Example.java
+ * @since Aug 17, 2011
+ * 
+ * @author Yoann Pigné
+
  */
 public class Example {
 
--- src-test/org/graphstream/stream/netstream/TestNetStream.java
+++ src-test/org/graphstream/stream/netstream/TestNetStream.java
@@ -1,13 +1,34 @@
-/**
+/*
+ * Copyright 2006 - 2011 
+ *     Julien Baudry	<julien.baudry@graphstream-project.org>
+ *     Antoine Dutot	<antoine.dutot@graphstream-project.org>
+ *     Yoann Pigné		<yoann.pigne@graphstream-project.org>
+ *     Guilhelm Savin	<guilhelm.savin@graphstream-project.org>
  * 
- * Copyright (c) 2010 University of Luxembourg
+ * This file is part of GraphStream <http://graphstream-project.org>.
  * 
- * @file TestNetStream.java
- * @date Aug 16, 2011
+ * GraphStream is a library whose purpose is to handle static or dynamic
+ * graph, create them from scratch, file or any source and display them.
  * 
- * @author Yoann Pigné
+ * This program is free software distributed under the terms of two licenses, the
+ * CeCILL-C license that fits European law, and the GNU Lesser General Public
+ * License. You can  use, modify and/ or redistribute the software under the terms
+ * of the CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
+ * URL <http://www.cecill.info> or under the terms of the GNU LGPL as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
  */
+
 package org.graphstream.stream.netstream;
 
 import static org.junit.Assert.assertEquals;
@@ -30,7 +51,15 @@ import org.graphstream.stream.thread.ThreadProxyPipe;
 import org.junit.Test;
 
 /**
- * Test of the NetStream protocol, sender and receiver
+ * Test of the NetStream protocol, sender and receiver.
+ * 
+ *  * Copyright (c) 2010 University of Luxembourg
+ * 
+ * TestNetStream.java
+ * @since Aug 16, 2011
+ * 
+ * @author Yoann Pigné
+ * 
  */
 public class TestNetStream {
 
