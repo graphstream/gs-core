52268c0326b087099ffbc7dfffb7b0862953dce9
gsavin <guilhelm.savin@graphstream-project.org>
2013-07-13 10:28:07 +0200

    Fix lat and lon in GPX.

--- src/org/graphstream/stream/file/FileSourceGPX.java
+++ src/org/graphstream/stream/file/FileSourceGPX.java
@@ -113,8 +113,8 @@ public class FileSourceGPX extends FileSourceXML {
 
 		void deploy() {
 			sendNodeAdded(sourceId, name);
-			sendNodeAttributeAdded(sourceId, name, "xyz", new double[] { lat,
-					lon, ele });
+			sendNodeAttributeAdded(sourceId, name, "xyz", new double[] { lon,
+					lat, ele });
 
 			for (String key : attributes.keySet())
 				sendNodeAttributeAdded(sourceId, name, key, attributes.get(key));
