cf68d078f592786435bb8369673fd99e4ecd331e
Sven Marquardt <Sven2q@hotmail.de>
2017-04-16 01:33:16 +0200

    Added new HttpServer
     new Api is now a rest Api. Test gives 100% coverage of the new class.

--- src-test/org/graphstream/stream/net/test/TestHTTPSource.java
+++ src-test/org/graphstream/stream/net/test/TestHTTPSource.java
@@ -1,57 +1,100 @@
 package org.graphstream.stream.net.test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
-import org.graphstream.graph.Edge;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
 import org.graphstream.stream.net.HTTPSource;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.hamcrest.core.Is.*;
-import static org.hamcrest.MatcherAssert.*;
 
 public class TestHTTPSource {
-	private final static String USER_AGENT = "Mozilla/5.0";
+	private static final String NODE_1 = "/node/1";
+
+	final static String ROOT_URL = "http://localhost:8080/g1";
+
+	static HTTPSource source;
+
+	static Graph graph;
+
+	/**
+	 * Create server and attach graph to it
+	 */
+	@BeforeClass
+	public static void setUp() {
+		source = new HTTPSource("g1", 8080);
+		graph = new DefaultGraph("g1");
+		source.addSink(graph);
+	}
+
+	
+
 	@Test
-	public void testServer() {
-		try {
-			HTTPSource source = new HTTPSource("localhost/g1", 8080);
-			Graph graph = new DefaultGraph("g1");
-			source.addSink(graph);
-			int edgeId = 0;
-			int nodeId=0;
-			graph.setStrict(false);
-			graph.setAutoCreate(true);
-			final Edge first = graph.addEdge(Integer.toString(edgeId++),Integer.toString(nodeId++),Integer.toString(nodeId++));
-			final Edge second = graph.addEdge(Integer.toString(edgeId++),Integer.toString(nodeId++),Integer.toString(nodeId++));
-			source.start();
-			assertThat(first.getId(), is("0"));
-			assertThat(second.getId(), is("1"));
-			URL url = new URL("http://localhost:8080/g1/edit?q=an&"+nodeId++);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("USER_AGENT", USER_AGENT);
-			String inputLine;
-			StringBuilder builder = new StringBuilder();
-			try(BufferedReader in= new BufferedReader(new InputStreamReader(con.getInputStream()))){
-				while((inputLine = in.readLine()) != null){
-					builder.append(inputLine);
-				}
-				in.close();
-				System.out.println(builder.toString());
-			}catch (Exception e) {
-				// TODO: handle exception
-			}
-			source.stop();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void testAddAndDeleteNode() throws IOException {
+		
+		
+		HttpUriRequest request = new HttpPost(ROOT_URL + NODE_1);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(1));
+		request = new HttpDelete(ROOT_URL + NODE_1);
+		httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(0));
+		
+	}
+
+	@Test
+	public void testAddAndDeleteEdge() throws ClientProtocolException, IOException {
+		HttpUriRequest request = new HttpPost(ROOT_URL + NODE_1);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/node/2");
+		httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/edge/1/1/2/false");
+		httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		assertThat(graph.getNodeCount(), is(2));
+		assertThat(graph.getEdgeCount(), is(1));
+		request = new HttpDelete(ROOT_URL + "/edge/1");
+		httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+	}
+	
+	@Test
+	public void testStep() throws ClientProtocolException, IOException {
+		HttpUriRequest request = new HttpPost(ROOT_URL + "/step/1");
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+		request = new HttpPost(ROOT_URL + "/step/A");
+		httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), is(400));
+	}
+	
+	
+	@After
+	public void cleanGraph(){
+		graph.clear();
+	}
+	
+	/*
+	 * Stop server
+	 */
+	@AfterClass
+	public static void cleanUp() {
+		source.stop();
 	}
 
 }
